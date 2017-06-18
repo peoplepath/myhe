2303da06c4092046318ec92981f9d959a4ad269f
Ondrej Esler <esler.ondrej@gmail.com>
2017-06-18 23:14:28 +0200

    add validate command

---
+++ src/Myhe/Command/ValidateCommand.php
@@ -0,0 +1,23 @@
+<?php
+namespace IW\Myhe\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ValidateCommand extends AbstractCommand
+{
+
+    protected function configure()
+    {
+        $this
+            ->setName('validate')
+            ->setDescription('Finds all YAMLs in directory and warns about syntax error');
+
+        parent::configure();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        iterator_to_array($this->find($input, $output));
+    }
+}
