e591748b9948cad519952fb481c1c9a091073bef
Ondrej Esler <esler.ondrej@gmail.com>
2017-08-10 21:54:40 +0200

    add "show values" command

---
+++ src/Myhe/Command/ShowCommand.php
@@ -0,0 +1,49 @@
+<?php
+namespace IW\Myhe\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class ShowCommand extends AbstractCommand
+{
+
+    protected function configure()
+    {
+        $this
+            ->setName('show')
+            ->setDescription('Show all values for each key');
+
+        parent::configure();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // aggregate by keys
+        $keys = [];
+        foreach ($this->find($input, $output) as list($filename, $key, $value)) {
+            $keys[implode('.', $key)][] = $value;
+        }
+
+        // show result
+        $io = new SymfonyStyle($input, $output);
+        if ($keys) {
+            foreach ($keys as $key => $values) {
+                $io->section($key);
+                $io->listing(
+                    array_unique(
+                        array_map(
+                            function ($value) {
+                                return print_r($value, true);
+                            },
+                            $values
+                        ),
+                        SORT_REGULAR
+                    )
+                );
+            }
+        } else {
+            $io->caution('No keys found');
+        }
+    }
+}
