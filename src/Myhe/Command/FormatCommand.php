9dee45a43eecf49a0ee7da7b6069c2a2610ce0df
Ondrej Esler <esler.ondrej@gmail.com>
2017-08-11 12:42:03 +0200

    add "format" command

---
+++ src/Myhe/Command/FormatCommand.php
@@ -0,0 +1,44 @@
+<?php
+namespace IW\Myhe\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Yaml\Yaml;
+
+class FormatCommand extends AbstractCommand
+{
+
+    protected function configure()
+    {
+        $this
+            ->setName('format')
+            ->setDescription('Format all matching yamls')
+            ->addOption(
+                'inline', 'l',
+                InputOption::VALUE_REQUIRED,
+                'Enable inline dump'
+            );
+
+        parent::configure();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // aggregate by filenames
+        $filenames = [];
+        foreach ($this->find($input, $output) as list($filename)) {
+            $filenames[$filename] = true;
+        }
+
+        $io = new SymfonyStyle($input, $output);
+        if ($io->confirm(sprintf('Do you really want format %d files?', count($filenames)))) {
+            // walk trough files and re-save them
+            foreach (array_keys($filenames) as $filename) {
+                $data = Yaml::parse(file_get_contents($filename));
+                file_put_contents($filename, Yaml::dump($data, $input->getOption('inline') ?? PHP_INT_MAX));
+            }
+        }
+    }
+}
